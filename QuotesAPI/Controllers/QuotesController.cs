4f2826c5157a845eb444651109e47a0a2e85eb5e
Jan Polzer <janman74@gmail.com>
2019-10-27 00:48:52 -0500

    Add Empty QuotesController

---
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotesAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuotesController : Controller
+    {
+        
+        
+    }
+}
