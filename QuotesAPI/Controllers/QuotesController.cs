ceacd57d727fbf6d96a575289827fa40af2ec01b
Jan Polzer <janman74@gmail.com>
2019-10-27 22:50:44 -0500

    Implement / hard code rest-full api

--- QuotesAPI/Controllers/QuotesController.cs
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -12,7 +12,34 @@ namespace QuotesAPI.Controllers
     [Route("api/[controller]")]
     public class QuotesController : Controller
     {
-        
-        
+        static List<Quote> quotes = new List<Quote>()
+        {
+            new Quote(){Id=0, Author="Einstein", Description="Imagination is more important than knoleadge", Title="Imagination" },
+            new Quote(){Id=1, Author="Tom Hardy", Description="If we still alive in the morninig we'll know we are not dead", Title="Alive in morning" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return quotes;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Quote quote)
+        {
+            quotes.Add(quote);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Quote quote)
+        {
+            quotes[id] = quote;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            quotes.RemoveAt(id);
+        }
     }
 }
