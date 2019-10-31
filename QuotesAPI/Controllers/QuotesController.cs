d130618d285edd558831c11639e0a455863a64b3
Jan Polzer <janman74@gmail.com>
2019-10-31 11:05:59 -0500

    Add sorting

--- QuotesAPI/Controllers/QuotesController.cs
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -23,9 +23,21 @@ namespace QuotesAPI.Controllers
 
         // GET: api/values
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string sort)
         {
-            var quotes = _quotesDbContext.Quotes;
+            IQueryable<Quote> quotes;
+            switch (sort)
+            {
+                case "desc":
+                    quotes = _quotesDbContext.Quotes.OrderByDescending(q => q.CreatedAt);
+                    break;
+                case "asc":
+                    quotes = _quotesDbContext.Quotes.OrderBy(q => q.CreatedAt);
+                    break;
+                default:
+                    quotes = _quotesDbContext.Quotes;
+                    break;
+            }
             return Ok(quotes);
         }
 
